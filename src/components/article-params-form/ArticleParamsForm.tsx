c4420d87f96d55ab61eb23114dbffec391d16e6f
Polina Khafizova <polina.hafizova04@mail.ru>
2026-02-21 00:31:46 +0300

    feat: выполнен шаг 2 и шаг 3

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,128 @@
+import { MouseEvent, useCallback } from 'react';
+import clsx from 'clsx';
+
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+
+	values: ArticleStateType;
+	onChange: (next: ArticleStateType) => void;
+
+	onToggle: () => void;
+	onClose: () => void;
+
+	onApply: () => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	values,
+	onChange,
+	onToggle,
+	onClose,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const handleSidebarClick = (e: MouseEvent) => {
+		e.stopPropagation();
+	};
+
+	const handleSelectChange = useCallback(
+		(key: keyof ArticleStateType) => (option: OptionType) => {
+			onChange({ ...values, [key]: option });
+		},
+		[onChange, values]
+	);
+
+	const handleApplySubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onApply();
+	};
+
+	const handleResetSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+
+			{isOpen && <div className={styles.overlay} onClick={onClose} />}
+
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				onClick={handleSidebarClick}>
+				<form
+					className={styles.form}
+					onSubmit={handleApplySubmit}
+					onReset={handleResetSubmit}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={values.fontFamilyOption}
+						options={fontFamilyOptions}
+						placeholder={defaultArticleState.fontFamilyOption.title}
+						onChange={handleSelectChange('fontFamilyOption')}
+					/>
+
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						selected={values.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={handleSelectChange('fontSizeOption')}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={values.fontColor}
+						options={fontColors}
+						placeholder={defaultArticleState.fontColor.title}
+						onChange={handleSelectChange('fontColor')}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={values.backgroundColor}
+						options={backgroundColors}
+						placeholder={defaultArticleState.backgroundColor.title}
+						onChange={handleSelectChange('backgroundColor')}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={values.contentWidth}
+						options={contentWidthArr}
+						placeholder={defaultArticleState.contentWidth.title}
+						onChange={handleSelectChange('contentWidth')}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
